bddabc9c774a1d587d07febd95d3017c3521d000
Sarthak Prajapati <sarthakprajapati@live.in>
2019-02-08 00:33:26 +0530

    content-header

---
+++ 2.js
@@ -0,0 +1,7 @@
+var http = require("http");
+
+http.createServer(function(req, res){
+    res.writeHead(200, {'Content-Type':'text/html'});
+    res.write("Hello My name is Sarthak");
+    res.end();
+}).listen(8056);
